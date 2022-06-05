279113894f41ae8a754bef1fed3d4bff011cd3d9
Rafael Guimarães dos Santos <rafaelguima@outlook.com>
2022-06-05 19:38:10 -0300

    Initial commit layout

---
+++ calc/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  calc
+//
+//  Created by rafael.guimaraes on 05/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        //Change the style and color a status bar, to use White font
+        return .lightContent
+    }
+
+
+}
+
